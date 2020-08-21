083730a8ab3162ffbc79b25b8ff36fdb1959ebc8
Tan <35444054+tanthai028@users.noreply.github.com>
2020-08-21 16:45:51 -0400

    Update character.py

--- character.py
+++ character.py
@@ -1,111 +1,118 @@
-import time, random, cmd, textwrap, os, sys
+import time, sys, os
 
-
-class character:
+# Defines person's age & name
+class person():
   def __init__(self):
     self.name = ''
     self.age = 0
-myCharacter = character()
 
-# Introduction
-def start():
-  os.system('clear')
-  question1()
+your = person()
+
+# Ask for name
+def name(questionName):
   
-def question1(): 
-  question1 = "Hi, what's your name?\n"
-  for character in question1:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.03)
-  character_name = input('> ')
-  myCharacter.name = character_name
-  confirmation1 = "Your name is " + myCharacter.name + "?" + " (Y/N)\n"
-  for character in confirmation1:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.03)
-  c1 = input("> ")
-  if c1.lower() == "y":
-    question2()
-  while c1.lower() == "n" or c1.lower() not in ["y", "n"]:
+  inputName = ''
+  while not inputName.isalpha():
+    for x in questionName:
+      sys.stdout.write(x)
+      sys.stdout.flush()
+      time.sleep(.03)
+
+    inputName = input("> ")
+    if not inputName.isalpha():
+      os.system('clear')
+      print("Invalid Input\n")
+    
+    your.name = inputName
+  
+# Ask for how old
+def age(questionAge):
+  inputAge = ''
+  while not inputAge.isdigit():
+    for x in questionAge:
+      sys.stdout.write(x)
+      sys.stdout.flush()
+      time.sleep(.03)
+    
+    inputAge = input("> ")
+    if not inputAge.isdigit():
+      os.system('clear')
+      print("Invalid Input\n")
+    
+    your.age = inputAge
+    
+# Baby / Child / Teenager / Adult / Old according age input
+def old():
+  your.age = int(your.age)
+  if your.age < 1:
     os.system('clear')
-    for character in question1:
-      sys.stdout.write(character)
-      sys.stdout.flush()
-      time.sleep(0.03)
-    character_name = input('>')
-    myCharacter.name = character_name
-    confirmation1 = "Your name is " + myCharacter.name + "?" + " (Y/N)\n"
-    for character in confirmation1:
-      sys.stdout.write(character)
-      sys.stdout.flush()
-      time.sleep(0.03)
-    c1 = input(">")
-    if c1.lower() == "y":
-      question2()
-
-
-
-
-
-def question2():
-  question2 = "Hi " + myCharacter.name + ", how old are you?\n"
-  for character in question2:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.03)
-  character_age = int(input('> '))
-  myCharacter.age = character_age
-  if character_age < 0:
     print('Invalid Age\n')
-    character_age = int(input('> '))
-  elif character_age <= 1:
-    baby = myCharacter.name + ', you are a baby\n'
+    age("How old are you %s?\n" % your.name.capitalize() )
+
+  elif your.age == 1:
+    baby = your.name.capitalize() + ', you are a baby.\n\n'
+    
     for character in baby:
       sys.stdout.write(character)
       sys.stdout.flush()
       time.sleep(0.03)
-  elif character_age < 13:
-    child = myCharacter.name + ', you are a child\n'
+
+  elif 1 <= your.age < 13:
+    child = your.name.capitalize() + ', you are a child.\n\n'
+    
     for character in child:
       sys.stdout.write(character)
       sys.stdout.flush()
       time.sleep(0.03)
-  elif character_age < 18:
-    teenager = myCharacter.name + ', you are a teenager\n'
+
+  elif 12 < your.age < 18:
+    teenager = your.name.capitalize() + ', you are a teenager.\n\n'
+    
     for character in teenager:
       sys.stdout.write(character)
       sys.stdout.flush()
       time.sleep(0.03)
-  elif character_age >= 21:
-    adult = myCharacter.name + ', you are an adult\n'
+
+  elif your.age >= 18 and your.age < 60:
+    adult = your.name.capitalize() + ', you are an adult.\n\n'
+    
     for character in adult:
       sys.stdout.write(character)
       sys.stdout.flush()
       time.sleep(0.03)
-  elif character_age > 60:
-    old = myCharacter.name + ', you are really old\n'
+
+  elif your.age >= 60:
+    old = your.name.capitalize() + ', you are really old.\n\n'
+    
     for character in old:
       sys.stdout.write(character)
       sys.stdout.flush()
       time.sleep(0.03)
-  summary()
-
-def summary():
+    
   time.sleep(1)
-  confirmation2 = ("\nSo your name is " + myCharacter.name + " and you are " + str(myCharacter.age) + " years old?" + " (Y/N)\n")
-  for character in confirmation2:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.03)
-  
-  c2 = input("> ")
-  if c2.lower() == "y":
-    sys.exit()
-  else:
-    start()
 
+# Confirmation about name & age
+def areyousure(questionConfirm):
+  inputConfirm = ''
+  while inputConfirm.lower() not in ['yes','no']:
+    for x in questionConfirm:
+      sys.stdout.write(x)
+      sys.stdout.flush()
+      time.sleep(.03)
+    
+    inputConfirm = input("> ")
+    if inputConfirm.lower() not in ['yes','no']:
+      os.system('clear')
+      print("Invalid Input\n")
+    elif inputConfirm.lower() == "yes":
+      return False
+    elif inputConfirm.lower() == "no":
+      return True
 
-start()
-
+# Starts program
+repeat = True
+while repeat:
+  name("Hi, what is your name?\n")
+  age("How old are you %s?\n" % your.name.capitalize() )
+  old()
+  repeat = areyousure("So your name is %s and you are %s years old? (yes/no)\n" % (your.name.capitalize(), your.age) ) 
